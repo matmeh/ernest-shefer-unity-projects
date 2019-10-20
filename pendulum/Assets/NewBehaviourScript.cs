03e911fbcdb1a46119ce122735f885e3dc8e78db
Ernest Shefer <shef-er@ya.ru>
2019-10-20 09:45:57 +0500

    initial commit

---
+++ pendulum/Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	
+
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = new Vector3(0, 10, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ pendulum/Assets/SpringJointScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringJointScript : MonoBehaviour {
+	
+	public float A = 0.01f;
+	public float k = 0.05f;
+	public float start_fase;
+	public float cyclic_frequency;
+
+	private float time;
+	private float coordY = 0;
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		cyclic_frequency = Mathf.Sqrt (k / rb.mass);
+		time = 0;
+		start_fase = - cyclic_frequency * time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		time = Time.realtimeSinceStartup;
+		//transform.translate.y = A * Mathf.Sin ( start_fase + cyclic_frequency * time );
+
+		coordY = A * Mathf.Cos (start_fase + cyclic_frequency * time);
+
+		rb.MovePosition ( new Vector3( 
+			0f,
+			coordY,
+			0f
+		));
+	}
+}
